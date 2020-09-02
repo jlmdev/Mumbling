a0c6417e02ea5f40d508f99382c2c76494e1564d
Josh Mann <jlmann72@gmail.com>
2020-09-02 14:35:56 -0400

    Method created, but cannot call from main yet

--- Program.cs
+++ Program.cs
@@ -2,11 +2,38 @@
 
 namespace Mumbling
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static String Accum(string s)
+        {
+            var output = "";
+            for (var index = 0; index < s.Length; index++)
+            {
+                var character = s[index];
+                var characterString = character.ToString();
+                for (var counter = 0; counter <= index + 1; counter++)
+                {
+                    if (counter == 0)
+                    {
+                        output += characterString.ToUpper();
+                    }
+                    else
+                    {
+                        output += characterString.ToLower();
+                    }
+                }
+                if (index != (s.Length - 1))
+                {
+                    output += "-";
+                }
+            }
+            return output;
+        }
+        static void Main(string[] args)
+        {
+            var result = Accum("abcd");
+        }
+
+
     }
-  }
 }
